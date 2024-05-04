87ce97a49dc1bd3829058d5ae40e31c6ce4bac8b
PRAKTIKAN <PRAKTIKAN@PC-3-0617>
2024-05-04 08:52:04 +0700

    Add project files.

---
+++ Jurnal10/Controllers/MahasiswaControllers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Jurnal10.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaControllers
+    {
+        private static List<Mahasiswa> listMahasiswa = new List<Mahasiswa>()
+        {
+            new Mahasiswa("Najwa Aulia Zahra", "1302220024", ["KPL", "PBO"], 46),
+            new Mahasiswa("Hasan Nurrahman Pane", "1302220033", ["Dasar Jaringan Komputer", "BASDAT"], 46),
+            new Mahasiswa("Ghifari Permata Alwy", "1302220031", ["Pengalaman Pengguna", "KWU"], 46)
+        };
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return listMahasiswa;
+        }
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return listMahasiswa[id];
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete (int id)
+        {
+            listMahasiswa.RemoveAt(id);
+        }
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa mahasiswa)
+        {
+            listMahasiswa.Add(mahasiswa);
+        }
+    }
+
+}
---
+++ Jurnal10/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace Jurnal10
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string NIM { get; set; }
+        public List<string> Course { get; set; }
+        public int Year { get; set; }
+
+        public Mahasiswa(string Nama, string NIM, List<string> Course, int Year)
+        {
+            this.Nama = Nama;
+            this.NIM = NIM;
+            this.Course = Course;
+            this.Year = Year;
+        }
+    }
+}
